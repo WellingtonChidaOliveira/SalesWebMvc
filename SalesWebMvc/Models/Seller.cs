94f5359da9d6023a86194279819121047f2122e3
Wellington Chida de Oliveira <wellingtonchida@protonmail.com>
2022-06-05 08:26:36 -0300

    Foreign key not null department in bd

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -11,9 +11,8 @@ namespace SalesWebMvc.Models
         public string Email { get; set; }
         public double BaseSalary { get; set; }
         public DateTime BirthDate { get; set; }
-
         public Department Department { get; set; }
-
+        public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller(int id, string name, string email, double baseSalary, DateTime birthDate, Department department)
